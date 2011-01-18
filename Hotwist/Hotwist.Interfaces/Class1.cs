e13ef4189233aaf64ee5d6954b4ee317830b8650
unknown <waldyr@.(none)>
2011-01-18 02:54:24 -0300

    primeiro checkin

---
+++ Hotwist/Hotwist.Interfaces/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hotwist.Interfaces
+{
+    public class Class1
+    {
+    }
+}
---
+++ Hotwist/Hotwist/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hotwist.MvcApplication" Language="C#" %>
