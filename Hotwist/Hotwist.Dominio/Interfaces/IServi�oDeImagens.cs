1d6f41b7aeb531bccfe5e6445b7e2ce994a346f3
Waldyr Felix <waldyrfelix@gmail.com>
2011-01-18 21:35:42 -0300

    Cria o de interfaces que vao constituir classes de servicos

---
+++ Hotwist/Hotwist.Dominio/Interfaces/IServi�oDeImagens.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Hotwist.Dominio.Interfaces
+{
+    interface IServiçoDeImagens
+    {
+        Stream GerarThumbnail(Stream imagemOriginal, Size proporção);
+        Stream ColocarLogoAImagem(Stream imagem, Stream logo, Posição posição);
+    }
+}
