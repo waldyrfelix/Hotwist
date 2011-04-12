28b82f5b9c0b3d9810fefa97e3b8653cd09c5896
Waldyr Felix <waldyrfelix@gmail.com>
2011-04-12 17:59:34 -0300

    Ajuste inicial do file upload (sem ajax)

---
+++ src/Hotwist.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hotwist.Web.Controllers;
+using System.Web.Mvc;
+using System.Web;
+using Moq;
+using System.Web.Routing;
+
+namespace Hotwist.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Quando_Subir_Logo_Gravar_Na_Pasta_Uploads()
+        {
+            // arrange
+            var mockPostedFile = new Mock<HttpPostedFileBase>();
+            mockPostedFile.Setup(postedFile => postedFile.SaveAs(@"C:\upload\logo.png"));
+            mockPostedFile.SetupGet(postedFile => postedFile.FileName).Returns("logo.png");
+
+            var mockServerUtil = new Mock<HttpServerUtilityBase>();
+            mockServerUtil.Setup(server => server.MapPath("~/Content/uploads"))
+                .Returns(@"C:\upload");
+
+            var mockHttpContext = new Mock<HttpContextBase>();
+            mockHttpContext.Setup(httpContext => httpContext.Server)
+                .Returns(mockServerUtil.Object);
+
+            var controller = new HomeController();
+            controller.ControllerContext = new ControllerContext(mockHttpContext.Object, new RouteData(), controller);
+
+            // act
+            controller.Upload(mockPostedFile.Object);
+
+            // assert
+            mockPostedFile.Verify(x => x.SaveAs(@"C:\upload\logo.png"), Times.Once());
+        }
+    }
+}
--- src/Hotwist.Web/Controllers/HomeController.cs
+++ src/Hotwist.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace Hotwist.Web.Controllers
 {
@@ -10,10 +11,15 @@ namespace Hotwist.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Teste = "uau funciona!";
-
             return View();
         }
 
+        public ActionResult Upload(HttpPostedFileBase logo)
+        {
+            string caminho = Server.MapPath("~/Content/uploads");
+            logo.SaveAs(Path.Combine(caminho, logo.FileName));
+
+            return new EmptyResult();
+        }
     }
 }
